5f12dbf6e460caa5308df7a7aedd7ebc488065ab
Norton Allen <allen@huarp.harvard.edu>
2023-04-07 11:31:01 -0400

    Added USB CDC Middleware

--- gds/main.c
+++ gds/main.c
@@ -1,9 +1,11 @@
-#include <gds_driver_init.h>
+#include "gds_driver_init.h"
+#include "usb_start.h"
 
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	system_init();
+  usb_init();
 
 	/* Replace with your application code */
 	while (1) {
